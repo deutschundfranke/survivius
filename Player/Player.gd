1c4bffb6034c7da71912952e9ebcc9e6c5b0c514
deutschundfranke <j.deutsch@deutschundfranke.de>
2024-07-25 22:40:29 +0200

    Update Player.gd
    player movement with acceleration

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,6 +5,8 @@ extends Area2D
 @export var weapons: Array[WeaponBase] = []
 
 var targetPos: Vector2
+var velocity: Vector2 = Vector2()  # Initial velocity
+@export var acceleration: float = 2000  # Acceleration rate in pixels per second squared
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -23,7 +25,9 @@ func _process(delta):
 		#if (self.position.y > 1000):
 			#self.position.y = 1000
 			
-	self.moveToTarget(delta)
+	# apply a more acceleration based movement
+	accelerate_towards_target(delta)
+	# self.moveToTarget(delta)
 	
 func setTargetPos(newTarget: Vector2):
 	self.targetPos = newTarget
@@ -39,7 +43,40 @@ func moveToTarget(delta):
 	
 	var maxSpeedNow = self.maxSpeed * delta
 	if (distance.length() > maxSpeedNow):
-		distance = distance.normalized() * maxSpeedNow
+		# distance = distance.normalized() * maxSpeedNow
+		distance = self.direction * maxSpeedNow
 	self.position = self.position + distance
 	
 	# clamp position
+
+func accelerate_towards_target(delta):
+	
+	# if target is reached, slow down
+	if (self.targetPos == Vector2(-1,-1)):
+		velocity -= velocity * 0.05
+		global_position += velocity * delta
+		return
+	
+	# Calculate the direction to the target
+	var direction_to_target = (self.targetPos - global_position).normalized()
+	
+	# Calculate the acceleration vector
+	var acceleration_vector = direction_to_target * acceleration * delta
+	
+	# Update the velocity
+	velocity += acceleration_vector
+	
+	# if target reached, set targetPos to "off"
+	var distance: Vector2 = (self.targetPos - self.position)
+	if (distance.length() < 100):
+		self.targetPos = Vector2(-1,-1)
+		
+	# Clamp the speed to the maximum value
+	if velocity.length() > maxSpeed:
+		velocity = velocity.normalized() * maxSpeed
+	
+	# Print the current velocity for debugging purposes
+	print("Current Velocity: ", velocity)
+	
+	# move as Node2D
+	global_position += velocity * delta
