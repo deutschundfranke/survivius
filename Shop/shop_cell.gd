55a83a0c60f6abeefa2e33213d75a3ed30576b33
Ben Deutsch <ben@bendeutsch.de>
2024-09-09 20:16:41 +0200

    make shop cells disappear when collected

--- Shop/shop_cell.gd
+++ Shop/shop_cell.gd
@@ -8,6 +8,9 @@ var height = 20
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.randomizeColor()
+	var area: Area2D = self.find_child("Area2D")
+	area.area_entered.connect(onAreaEntered)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -25,3 +28,7 @@ func setColor(color: Color):
 func randomizeColor():
 	var color = Color(randf_range(0, 1.0), randf_range(0, 1.0), randf_range(0, 1.0))
 	self.setColor(color)
+
+func onAreaEntered(area: Area2D):
+	if area.is_in_group("Player"):
+		self.collected.emit(self)
